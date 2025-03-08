01394057eaa9bf2eef43da118b8dd07d7c2f0e82
cnwang <cnwang08@gmail.com>
2025-03-08 22:39:45 +0800

    Update log files and application properties

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.service.LikeService;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.ui.Model;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
@@ -17,13 +19,16 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index",method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page){
@@ -38,6 +43,10 @@ public class HomeController {
                 map.put("post",post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user",user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST,post.getId());
+                map.put("likeCount",likeCount);
+
                 discussPosts.add(map);
             }
         }
